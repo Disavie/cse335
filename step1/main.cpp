fec8a2aa000864f61550bdf3e890dce1de135280
davis stermer <davisstermer@gmail.com>
2026-01-15 09:58:43 -0500

    began working on step1

---
+++ step1/main.cpp
@@ -0,0 +1,19 @@
+/**
+* @file main.cpp
+ * @author Davis Stermer
+ *
+ * Program main entry point
+ */
+
+#include <iostream>
+using namespace std;
+/**
+ * Main entry point
+ *
+ * This is where the program starts.
+ * @return
+ */
+int main(){
+    cout << "!!!Hello World!!!" << endl;
+    return 0;
+}
